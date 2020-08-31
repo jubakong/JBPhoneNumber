7a8f261aca720547ae06cadf981804b78c034ea5
jubakong <jubakong@gmail.com>
2020-08-31 09:40:12 +0900

    replace me file test

---
+++ Example/Pods/Target Support Files/JBPhoneNumber/JBPhoneNumber-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JBPhoneNumber : NSObject
+@end
+@implementation PodsDummy_JBPhoneNumber
+@end
---
+++ Example/Pods/Target Support Files/Pods-JBPhoneNumber_Example/Pods-JBPhoneNumber_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JBPhoneNumber_Example : NSObject
+@end
+@implementation PodsDummy_Pods_JBPhoneNumber_Example
+@end
